52795ec120e869e03c5c44dc87ba87ef9d8877e0
Erick Boch <erickboch67@gmail.com>
2022-04-01 17:46:53 -0600

    API REST JAVA CON SPRING BOOT

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/proyecto-usuarios'));
-app.get('/*', function(req,res)
-
-{res.sendFile(path.join(__dirname+'/dist/proyecto-usuarios/index.html'));});
+app.use(express.static('./dist/prueba-heroku'));
+app.get('/*', (req,res)=>{
+    res.sendFile('index.html',{root:'/dist/prueba-heroku/'});
+});
 
 app.listen(process.env.PORT || 8080);
