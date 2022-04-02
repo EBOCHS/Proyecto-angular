4bda451ed1db6581eeeabb93fc5d434bd86b5ab7
Erick Boch <erickboch67@gmail.com>
2022-04-01 20:46:54 -0600

    PROYECTO ANGULAR

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/prueba-heroku'));
-app.get('/*', (req,res)=>{
-    res.sendFile('index.html',{root:'/dist/prueba-heroku/'});
-});
-
+app.use(express.static(__dirname + '/dist/proyecto-usuarios'));
+app.get('/*', function(req,res) 
+{res.sendFile(path.join(__dirname+'/dist/proyecto-usuarios/index.html'));});
 app.listen(process.env.PORT || 8080);
